911406c2fa4446391e36e7d10b0568ec14f57fb0
marianaizq <mariana.izquierdo@iteso.mx>
2024-11-02 16:50:06 -0600

    commit

--- dia_anterior.py
+++ dia_anterior.py
@@ -1,3 +1,40 @@
 """
-Codifica tu solución en este archivo.
+Tarea:    Siguiente día
+Autor:  Mariana Izquierdo Preciado
+Expediente: 756355
+Carrera: Ingeniería y Ciencia de Datos 
+
+Materia: Algoritmos y Programación
+Profesor: Jorge A. Zaldívar Carrillo
+Grupo:    ESI232B2
+
+Fecha:    2/nov/2024
+Tiempo aproxumado de elaboración: 1 hora
 """
+#importar
+from dia_siguiente import dias_del_mes
+
+# Programa principal
+def main():
+    #Entradas
+    dia = int(input("Día: "))
+    mes = int(input("Mes: "))
+    anho = int(input("Año: "))
+
+    #Proceso
+    dia -=1
+    if dia == 0:
+        mes -=1
+        if mes == 0:
+            mes=12
+            anho-=1
+        dia=dias_del_mes(mes,anho)
+
+    #Salidas
+    print()
+    print("Dia: ", dia)
+    print("Mes: ", mes)
+    print("Año: ", anho)
+
+if __name__== "__main__":
+    main()
